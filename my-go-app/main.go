efab86ea92149c7410832a2289208b519ce24735
Nur Imam Iskandar <36033209+iskandar45@users.noreply.github.com>
2025-03-26 06:51:23 +0000

    Variable & Type Data

--- my-go-app/main.go
+++ my-go-app/main.go
@@ -4,4 +4,8 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello, GitHub Codespaces!")
+	fmt.Println("++++++++++++++++++++++++++")
+	fmt.Println("1. Variable & Type Data")
+	type_data()
+	fmt.Println("++++++++++++++++++++++++++")
 }
---
+++ my-go-app/type_data.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func type_data() {
+	var name string = "Nur Imam"
+	var age int = 25
+	var isMarried bool = true
+	heigth := 170.6
+
+	fmt.Println("Name: ", name)
+	fmt.Println("Age: ", age)
+	fmt.Println("Is Married: ", isMarried)
+	fmt.Println("Heigth: ", heigth)
+}
+
+// func main() {
+// 	type_data()
+// }
